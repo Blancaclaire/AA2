ae8fb0e880fac0138a895cc2646288b28559ac1c
Blancaclaire <clara.alonso.jimenez@gmail.com>
2026-02-25 13:11:35 +0100

    Añadidos componentes de visualizacion de cursos heredados de capa publica

--- frontend/vue-project/src/views/Admin/CoursesView.vue
+++ frontend/vue-project/src/views/Admin/CoursesView.vue
@@ -2,9 +2,20 @@
 import CourseForm from '@/components/adminLayout/courses/CourseForm.vue';
 import { useCourseStore } from '@/stores/CoursesStore';
 import { useAuthStore } from '@/stores/AuthStore'
+import CourseFilters from '@/components/publicLayout/home/CoursesFilter.vue'
+import ListCoursesComponent from '@/components/publicLayout/home/ListCoursesComponent.vue';
+import { onMounted } from 'vue';
 
+const courseStore = useCourseStore()
 
+onMounted(() => {
+    courseStore.getCourses()
+})
 const authStore = useAuthStore()
+
+const onFilter = (params: any) => {
+  courseStore.getCourses(params)
+}
 
 </script>
 
@@ -12,4 +23,7 @@ const authStore = useAuthStore()
   <h1>Gestión de Cursos</h1>
 
   <CourseForm></CourseForm>
+  <h2>Cursos disponibles</h2>
+  <CourseFilters @filter="onFilter" />
+  <ListCoursesComponent :courses="courseStore.courses"></ListCoursesComponent>
 </template>
